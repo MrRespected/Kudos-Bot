1d7ac6fc10a98c30f49f9695106db05123c8f124
Kudos <77716214+Kudos-Official@users.noreply.github.com>
2021-07-19 07:59:05 +0530

    Update Rank.js

--- Commands/Levelling/Rank.js
+++ Commands/Levelling/Rank.js
@@ -10,7 +10,7 @@ example:"rank Stickman",
 explain:"Get the rank of the user stickman!",
 code:`$if[$message==]
 $author[Here is the rank of $usertag[$get[user]]!;$useravatar[$get[user]]]
-$image[https://vacefron.nl/api/rankcard?username=$username&avatar=$authorAvatar&level=8&rank=2&currentxp=350&nextlevelxp=400&previouslevelxp=150&custombg=https://cdn.discordapp.com/attachments/740416020761804821/741310178330148894/1596811604088.png&xpcolor=FFFFFF&isboosting=true]
+$image[https://api.no-api-key.com/api/v2/rank/2?current=$getUserVar[xp;$get[user]]&total=$getUserVar[req;$get[user]]&rank=$getleaderboardinfo[level;$get[user];user;top]&level=$getUserVar[level;$get[user]]&discrim=$discriminator[$get[user]]&username=$replaceText[$username[$get[user]]; ;+;-1]&avatar=$replacetext[$useravatar[$get[user]]?size=4096;.webp;.png]&status=$status[$get[user]]&barFill=GREY&mainColor=YELLOW]
 $color[RANDOM]
 $footer[Position: $getleaderboardinfo[level;$get[user];user;top] | Requested by $usertag;$authoravatar]
 $addtimestamp
