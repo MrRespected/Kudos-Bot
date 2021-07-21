11b3a851d414ec5cd2c558a5eee7b07f928ab86f
Kudos <77716214+Kudos-Official@users.noreply.github.com>
2021-07-21 20:53:43 +0530

    Deleted utility

--- Commands/Utility/Mass Mention Always Execute.js
+++
@@ -1,6 +0,0 @@
-module.exports = ({
-name:"$alwaysExecute",
-code:`$sendmessage[<@$authorid>, do not mass mention!;no]
-$onlyif[$mentioned[4]!=;]
-$onlyif[$hasperms[$authorid;managemessages]==false;]
-$onlyif[$getservervar[massmention]==true;]`})
